8aa6450d7b316b27b3b76f32e8ce841ff9a8f0b1
DamianMarvel7 <110451781+DamianMarvel7@users.noreply.github.com>
2023-04-14 22:11:47 +0700

    point hiks

---
+++ app/src/main/java/com/simplicity/Point.java
@@ -0,0 +1,26 @@
+public class Point {
+    private int x = 0;
+    private int y = 0;
+    // a constructor!
+    public Point(int a, int b) {
+    x = a; y = b;
+    }
+
+    public int getX(){
+        return x;
+    }
+
+    public int getY(){
+        return y;
+    }
+
+    public void setX(int ax){
+        x=ax;
+    }
+
+    public void setY(int ay){
+        y=ay;
+    }
+
+}
+
