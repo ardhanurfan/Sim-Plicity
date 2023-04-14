19c1056d1a30a7de445487e8910f1b0900fc74ca
ardhanurfan <ardhanurfan@gmail.com>
2023-04-14 21:00:54 +0700

    update: sim garis besar aksi

--- app/src/main/java/com/simplicity/Objek/ObjekMakanan.java
+++ app/src/main/java/com/simplicity/Objek/ObjekMakanan.java
@@ -1,19 +1,17 @@
 package com.simplicity.Objek;
 
-import java.util.Arrays;
-
 public class ObjekMakanan extends Objek {
-    String[] bahan;
+    ObjekBahanMakanan[] bahan;
     int kekenyangan;
 
-    public ObjekMakanan(String nama_makanan, String[] bahan, int kekenyangan){
+    public ObjekMakanan(String nama_makanan, ObjekBahanMakanan[] bahan, int kekenyangan){
         super(nama_makanan);
         this.bahan = bahan;
         this.kekenyangan = kekenyangan;
     }
 
     // getter
-    public String[] getBahan(){
+    public ObjekBahanMakanan[] getBahan(){
         
         return bahan;
     }
@@ -28,7 +26,7 @@ public class ObjekMakanan extends Objek {
     public void printBahan(){
         System.out.print(bahan[0]);
         for (int i = 1; i < bahan.length; i++){
-            System.out.print("+" + bahan[i]);
+            System.out.print("+" + bahan[0]);
         }
     }
 
@@ -51,27 +49,4 @@ public class ObjekMakanan extends Objek {
             daftar_makanan[i].print();
         }
     }
-
-    // testing
-    public static void main(String[] args){
-        // inisialisasi di bawah ini di letakan di MAIN
-
-        // Inisiasi array daftar makanan dengan tipe objek_makanan
-        ObjekMakanan[] daftar_makanan = new ObjekMakanan[5];
-        daftar_makanan[0] = new ObjekMakanan("Nasi Ayam", new String[]{"Nasi", "Ayam"}, 16);
-        daftar_makanan[1] = new ObjekMakanan("Nasi Kari", new String[]{"Nasi", "Kentang", "Wortel", "Sapi"}, 30);
-        daftar_makanan[2] = new ObjekMakanan("Susu Kacang", new String[]{"Susu", "Kacang"}, 5);
-        daftar_makanan[3] = new ObjekMakanan("Tumis Sayur", new String[]{"Wortel", "Bayam"}, 5);
-        daftar_makanan[4] = new ObjekMakanan("Bistik\t", new String[]{"Kentang","Sapi"}, 22);
-        // print daftar makanan
-        //printArray(daftar_makanan); 
-
-        // print daftar bahan dari salah satu menu
-        String[] bahan1 = daftar_makanan[0].getBahan();
-        System.out.println(Arrays.toString(bahan1));
-
-        // print salah satu bahan dari salah satu menu
-        System.out.println(daftar_makanan[0].getBahan()[1]);
-        
-    }    
 }
